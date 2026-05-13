8bd8aaf948fa5cce7dd0cee9b5bd0ee23a5efc48
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-05-13 15:43:09 -0400

    feat: Initialize observables module with Gas and Liquid Batch Reactor observables

---
+++ pyreactsim/observables/__init__.py
@@ -0,0 +1,7 @@
+from .gas_br import GasBatchReactorObservables
+from .liquid_br import LiquidBatchReactorObservables
+
+__all__ = [
+    "GasBatchReactorObservables",
+    "LiquidBatchReactorObservables",
+]
