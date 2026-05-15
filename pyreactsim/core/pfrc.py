e38a1793f2a6927e320308c78ef63c0ba60587a0
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-05-15 01:19:36 -0400

    feat: Add options for gas mixture total heat capacity, liquid mixture volumetric heat capacity, and reaction enthalpy mode in PFR reactor initialization

--- pyreactsim/core/pfrc.py
+++ pyreactsim/core/pfrc.py
@@ -63,6 +63,9 @@ class PFRReactorCore(ReactorCore):
         self.gas_heat_capacity_mode = pfr_reactor_options.gas_heat_capacity_mode
         self.liquid_heat_capacity_mode = pfr_reactor_options.liquid_heat_capacity_mode
         self.liquid_density_mode = pfr_reactor_options.liquid_density_mode
+        self.use_gas_mixture_total_heat_capacity = pfr_reactor_options.use_gas_mixture_total_heat_capacity
+        self.use_liquid_mixture_volumetric_heat_capacity = pfr_reactor_options.use_liquid_mixture_volumetric_heat_capacity
+        self.reaction_enthalpy_mode = pfr_reactor_options.reaction_enthalpy_mode
 
         # SECTION: process model configuration
         # ! T_in: inlet temperature [K]
