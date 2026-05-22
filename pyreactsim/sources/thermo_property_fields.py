39b3ac7a987de2372b377023a36898b81d6a776d
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-05-22 13:58:45 -0400

    feat: Update ThermoPropertyFields to include dCp_rxns and dH_rxns_298 attributes

--- pyreactsim/sources/thermo_property_fields.py
+++ pyreactsim/sources/thermo_property_fields.py
@@ -9,47 +9,54 @@ class ThermoPropertyFields:
     """
     Typed attribute declarations for dynamically assigned thermodynamic properties.
     """
+    # ! heat capacity at constant pressure (Cp_IG) for ideal gas components
     Cp_IG: np.ndarray
     Cp_IG_comp: Dict[str, float]
     Cp_IG_src: Dict[str, ComponentEquationSource]
 
+    # ! heat capacity at constant pressure (Cp_LIQ) for liquid components
     Cp_LIQ: np.ndarray
     Cp_LIQ_comp: Dict[str, float]
     Cp_LIQ_src: Dict[str, ComponentEquationSource]
 
+    # ! liquid density (rho_LIQ) for liquid components
     rho_LIQ: np.ndarray
     rho_LIQ_comp: Dict[str, float]
     rho_LIQ_src: Dict[str, ComponentEquationSource]
 
+    # ! ideal gas formation enthalpy at 298 K (EnFo_IG_298) for ideal gas components
     EnFo_IG_298: np.ndarray
     EnFo_IG_298_comp: Dict[str, float]
     EnFo_IG_298_src: Dict[str, Any]
 
+    # ! liquid formation enthalpy at 298 K (EnFo_LIQ_298) for liquid components
+    EnFo_LIQ_298: np.ndarray
+    EnFo_LIQ_298_comp: Dict[str, float]
+    EnFo_LIQ_298_src: Dict[str, Any]
+
+    # ! molecular weight (MW) for components
     MW: np.ndarray
     MW_comp: Dict[str, float]
     MW_src: Dict[str, Any]
 
+    # ! reaction enthalpy (dH_rxns) for reactions in the reactor, calculated based on the current temperature and reaction enthalpy mode
     dH_rxns: Optional[Dict[str, Dict[str, Any]]]
+
+    # ! mixture density for liquid phase (rho_LIQ_MIX)
     rho_LIQ_MIX: Optional[CustomProp]
+
+    # ! total heat capacities for gas mixture
     Cp_IG_MIX_TOTAL: Optional[CustomProp]
+    # ! total heat capacities for liquid mixture
     Cp_LIQ_MIX_TOTAL: Optional[CustomProp]
+    # ! volumetric heat capacity for liquid mixture
     Cp_LIQ_MIX_VOLUMETRIC: Optional[CustomProp]
 
+    # ! average heat capacity change for reactions (dCp_rxns)
     dCp_rxns: np.ndarray
-    dH_rxns_298: np.ndarray
 
-    thermo_properties: List[str] = [
-        "Cp_IG",
-        "Cp_LIQ",
-        "rho_LIQ",
-        "EnFo_IG_298",
-        "MW",
-        "dH_rxns",
-        "rho_LIQ_MIX",
-        "Cp_IG_MIX_TOTAL",
-        "Cp_LIQ_MIX_TOTAL",
-        "Cp_LIQ_MIX_VOLUMETRIC",
-    ]
+    # ! enthalpy of reactions at 298 K (dH_rxns_298)
+    dH_rxns_298: np.ndarray
 
     def __init__(self) -> None:
         # NOTE: default-initialize all optional runtime attributes so missing
